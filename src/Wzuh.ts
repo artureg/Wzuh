0786b6859a65fd07b6378e32dd7fe81ae427b476
Artur Paliashchuk <artureg@Arturs-MacBook-Pro.local>
2023-11-15 22:02:28 +0100

    init

--- src/Wzuh.ts
+++ src/Wzuh.ts
@@ -5,9 +5,7 @@ export function Wzuh<T extends { new (...args: any[]): {} }>(constructor: T) {
 
       const params = args.length > 0 ? args[0] : null;
       if (params && typeof params === 'object') {
-        console.log(constructor)
         for (const key of Object.getOwnPropertyNames(params)) {
-          //console.log(key)
           if (params.hasOwnProperty(key)) {
             this[key] = params[key];
           }
