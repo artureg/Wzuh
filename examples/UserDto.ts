8c314b34a515c76dd830d1b03355022b7812765d
Ilya Shavel <ilya.shavel@bagsporter.com>
2023-11-20 11:48:54 +0100

    Wzuh+mapable

--- examples/UserDto.ts
+++ examples/UserDto.ts
@@ -1,20 +1,27 @@
 import {UserEntity} from "./UserEntity";
 import {Wzuh} from "../src";
+import { Mapable, WzuhParams } from '../src/Wzuh';
 
-@Wzuh({
-  firstName: "lastName",
-  lastName: "firstName",
-})
+@Wzuh()
 export class UserDto {
-  constructor(params?: UserEntity | { email: string; password: string; }) {}
+  constructor(@WzuhParams params?: UserEntity | { email: string; password: string; }) {}
 
+  @Mapable()
   email!: string;
+  @Mapable()
   firstName!: string;
+  @Mapable('firstName')
   lastName!: string;
+  @Mapable()
   phone!: string;
+  @Mapable()
   country!: string;
+  @Mapable()
   city!: string;
+  @Mapable()
   address!: string;
+  @Mapable()
   postalCode!: string;
+  @Mapable()
   birthDate!: Date;
 }
--- src/Wzuh.ts
+++ src/Wzuh.ts
@@ -1,27 +1,32 @@
-export function Wzuh(
-  mapping?: { [key: string]: string }
-) {
+export function Wzuh() {
   return function<T extends { new (...args: any[]): {} }>(constructor: T) {
-    const keysMap = new Map<string, string>();
-    if (mapping) {
-      for (const key of Object.getOwnPropertyNames(mapping)) {
-        keysMap.set(key, mapping[key]);
-      }
-    }
+    const keysMap = constructor.prototype.keysMap as Map<string, string> || undefined;
     return class extends constructor {
       constructor(...args: any[]) {
         super();
-
         const params = args.length > 0 ? args[0] : null;
-        if (params && typeof params === 'object') {
-          for (const key of Object.getOwnPropertyNames(params)) {
-            const thisKey = keysMap.get(key) || key;
-            if (params.hasOwnProperty(thisKey)) {
-              this[thisKey] = params[key];
+        if (keysMap && params && typeof params === 'object') {
+          keysMap.forEach((mapFrom, key) => {
+            if (params.hasOwnProperty(mapFrom)) {
+              this[key] = params[mapFrom];
             }
-          }
+          })
         }
       }
     };
   }
+}
+
+export function Mapable(mapFrom?: string) {
+  return function(target: any, key: string) {
+    if (!target.constructor.prototype.keysMap) {
+      target.constructor.prototype.keysMap = new Map<string, string>();
+    }
+    target.constructor.prototype.keysMap.set(key, mapFrom || key);
+    // console.log(`${key} type: ${target.constructor.name}`);    
+  }
+}
+
+export function WzuhParams(target: Object, propertyKey: string | symbol, parameterIndex: number) {
+  console.log(2);
 }
